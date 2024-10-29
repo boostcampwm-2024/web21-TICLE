5d8117eba6cfc796a47ca8587e639dfa45b9ed72
seoko97 <sukho1007@naver.com>
2024-10-29 23:22:27 +0900

    feat: eslint 공통 설정 추가
    - naming convention 설정 추가
    - import/order 설정 추가
    - typescript 설정 추가
    - prettier 설정 추가

---
+++ packages/eslint/paths.js
@@ -0,0 +1,48 @@
+// packages/eslint/rules/paths.js
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ['./tsconfig.json'],
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+          'object',
+          'type',
+        ],
+        pathGroups: [
+          {
+            pattern: '@repo/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        distinctGroup: false,
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
