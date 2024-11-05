00d9ce5838d6b24cbe1c60024d8a7c80ebaa37c2
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-05 13:53:35 +0900

    feat: global.css에 색상 토큰 설정

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,7 +1,14 @@
+import plugin from 'tailwindcss/plugin';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Pretendard', ...fontFamily.sans],
+    },
+  },
 };
 
 export default config;
