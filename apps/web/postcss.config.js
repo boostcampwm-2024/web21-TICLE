8e3ec8cd36b24df0055cdb538032d3da49c27d95
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-05 15:34:23 +0900

    feat: tailwind theme extend에 시맨틱 컬러 추가

--- apps/web/postcss.config.js
+++ apps/web/postcss.config.js
@@ -1,8 +1,6 @@
-export const config = {
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-
-export default config;
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,12 +1,40 @@
-import plugin from 'tailwindcss/plugin';
 import { fontFamily } from 'tailwindcss/defaultTheme';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/*.tsx',
+  ],
   theme: {
     fontFamily: {
       sans: ['Pretendard', ...fontFamily.sans],
+    },
+    extend: {
+      backgroundColor: {
+        primary: 'var(--purple-500)',
+        secondary: 'var(--purple-200)',
+        teritary: 'var(--purple-100)',
+        alt: 'var(--grey-500)',
+        weak: 'var(--grey-50)',
+        white: 'var(--white)',
+        black: 'var(--black)',
+        overlay: 'var(--black-alpha-60)',
+      },
+      textColor: {
+        main: 'var(--black)',
+        alt: 'var(--grey-900)',
+        weak: 'var(--grey-700)',
+        primary: 'var(--purple-500)',
+        white: 'var(--white)',
+        error: 'var(--red)',
+      },
+      borderColor: {
+        main: 'var(--grey-300)',
+        primary: 'var(--purple-500)',
+        error: 'var(--red)',
+      },
     },
   },
 };
