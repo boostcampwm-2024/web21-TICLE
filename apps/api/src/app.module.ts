5969447b7a34f14516bcff45d7fc4f54664aebdf
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-05 18:46:47 +0900

    feat: typeORM 모듈 main에 import

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from '@/app.controller';
 
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { dbConfig } from './config/dbConfig';
 import { StreamModule } from './stream/stream.module';
 import { TicleModule } from './ticle/ticle.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, TicleModule, StreamModule, UserModule],
+  imports: [AuthModule, TicleModule, StreamModule, UserModule, TypeOrmModule.forRoot(dbConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
