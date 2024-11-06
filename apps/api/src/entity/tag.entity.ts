ab33220aa5e51dc8c9765d64ac21f4c6e6519e73
LEE JIEUN <jieun3595@soongsil.ac.kr>
2024-11-06 11:30:20 +0900

    feat: tag Entity 생성

---
+++ apps/api/src/entity/tag.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToMany } from 'typeorm';
+
+import { Ticle } from './ticle.entity';
+
+@Entity('tag')
+export class Tag {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column('varchar')
+  name: string;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', name: 'created_at' })
+  createdAt: Date;
+
+  @ManyToMany(() => Ticle, (ticle) => ticle.tags)
+  ticles: Ticle[];
+}
