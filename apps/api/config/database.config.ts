01e45d609ed8c5d8fa46d7c98b133efb7b7e4825
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-06 09:56:01 +0900

    style: db config 파일의 불필요한 코드  제거

--- apps/api/config/database.config.ts
+++ apps/api/config/database.config.ts
@@ -3,9 +3,9 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', async () => ({
   type: process.env.DATABASE_TYPE || 'mysql',
-  host: process.env.DATABASE_HOST || 'localhost',
+  host: process.env.DATABASE_HOST,
   port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
-  username: process.env.DATABASE_USERNAME || 'myusername1',
-  password: process.env.DATABASE_PASSWORD || 'mypassword',
-  name: process.env.DATABASE_NAME || 'mydatabases',
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  name: process.env.DATABASE_NAME,
 }));
