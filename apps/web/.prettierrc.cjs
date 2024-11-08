f5fc9c2da1616f9eac1b8a7ec96994e3e9cdab6c
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-08 23:51:21 +0900

    feat: prettier 설정

---
+++ apps/web/.prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 100,
+  useTabs: false,
+  semi: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  singleQuote: true,
+  tabWidth: 2,
+};
