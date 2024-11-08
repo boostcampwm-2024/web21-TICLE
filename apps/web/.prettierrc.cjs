cfb5742f4ff0e8b478ef5841018d10b9f73b5377
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-09 01:26:40 +0900

    feat: web 내 prettierrc에서 root prettierrc를 import하도록 변경

--- apps/web/.prettierrc.cjs
+++ apps/web/.prettierrc.cjs
@@ -1,12 +1,6 @@
+const rootConfig = require('../../.prettierrc.js');
+
 module.exports = {
+  ...rootConfig,
   plugins: ['prettier-plugin-tailwindcss'],
-  printWidth: 100,
-  useTabs: false,
-  semi: true,
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  bracketSameLine: false,
-  arrowParens: 'always',
-  singleQuote: true,
-  tabWidth: 2,
 };
