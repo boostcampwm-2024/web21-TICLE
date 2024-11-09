d223a5a440e88ed00ca1f2962559524537b78fcf
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-09 20:03:46 +0900

    feat: userService의 createUser, findUser 생성

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -8,7 +8,7 @@ export class AuthService {
   constructor(private userService: UserService) {}
 
   async validateUser(username: string, password: string): Promise<any> {
-    const user = await this.userService.findOne(username); // user 태이블에 username이 있는지 확인, 있으면 user 객체 반환
+    const user = await this.userService.findUser(username); // user 태이블에 username이 있는지 확인, 있으면 user 객체 반환
     if (user) {
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (isPasswordValid) {
