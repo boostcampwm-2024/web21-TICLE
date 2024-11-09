7c0080377ba40cfabee53c1bd36dc962c84091d3
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-09 19:22:19 +0900

    feat: 로그인 service 로직 구현
    1. bcrypt를 통해 해싱된 비번과 일치하는지 검사
    2. 일치 시 user객체 반환
    3. 불일치시 null 반환

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+import { UserService } from '@/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(username); // user 태이블에 username이 있는지 확인, 있으면 user 객체 반환
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (isPasswordValid) {
+        const { password, ...result } = user;
+        return result;
+      }
+    }
+    return null;
+  }
+}
