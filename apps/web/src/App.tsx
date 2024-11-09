69191e15092036efa54aab546fa1eb79193328eb
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-10 06:13:13 +0900

    fix: lint오류 수정

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,7 @@
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 
 import { routeTree } from './routeTree.gen';
+
 const router = createRouter({ routeTree });
 declare module '@tanstack/react-router' {
   interface Register {
