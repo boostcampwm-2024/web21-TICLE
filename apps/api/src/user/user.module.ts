224bffd9458e9acb2c21715837b024b105705832
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-09 18:14:14 +0900

    feat: passport에 사용할 기본 파일생성 및 의존성 연결

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@/entity/user.entity';
 
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
   imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService],
 })
 export class UserModule {}
