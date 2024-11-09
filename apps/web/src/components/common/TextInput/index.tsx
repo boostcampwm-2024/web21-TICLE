743395b8bea607df0aba4d1447f568470d8a0cd4
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-10 07:16:43 +0900

    design: 기본 스타일 정의

--- apps/web/src/components/common/TextInput/index.tsx
+++ apps/web/src/components/common/TextInput/index.tsx
@@ -9,22 +9,34 @@ interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 function TextInput(
-  { label, description, errorMessage, required, type = 'text', ...props }: TextInputProps,
+  {
+    label,
+    description,
+    errorMessage,
+    required,
+    type = 'text',
+    className,
+    ...props
+  }: TextInputProps,
   ref: Ref<HTMLInputElement>
 ) {
   return (
-    <div>
+    <div className="flex flex-col gap-1.5">
       {label && (
-        <label>
+        <label className="text-title2 text-main">
           {label}
-          {required && '*'}
+          {required && <span className="text-error">{' *'}</span>}
         </label>
       )}
-      {description && <p>{description}</p>}
-      <div>
-        <input {...props} type={type} ref={ref} required={required} />
-        {errorMessage && <span>{errorMessage}</span>}
-      </div>
+      {description && <p className="text-body3 text-alt">{description}</p>}
+      <input
+        {...props}
+        type={type}
+        ref={ref}
+        required={required}
+        className={`${className} ${errorMessage && 'focus:border-error'} boder-main h-11 w-full rounded border border-main bg-white px-3.5 py-2.5 text-body1 text-main placeholder:text-weak focus:border-primary focus:outline-none`}
+      />
+      {errorMessage && <span className="text-label1 text-error">{errorMessage}</span>}
     </div>
   );
 }
