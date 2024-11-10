ebe2124bc2862753f3efc8e33e31f5a871e0cec9
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-11 04:11:39 +0900

    feat: Dialog Root 구현

--- apps/web/src/components/common/Dialog/index.tsx
+++ apps/web/src/components/common/Dialog/index.tsx
@@ -1,4 +1,8 @@
 import { ReactNode } from '@tanstack/react-router';
+import { useRef } from 'react';
+
+import useOutsideClick from '@/hooks/useOutsideClick';
+import cn from '@/utils/cn';
 
 import Portal from '../Portal';
 
@@ -10,12 +14,20 @@ interface DialogRootProps {
 }
 
 function DialogRoot({ isOpen, onClose, children, className }: DialogRootProps) {
+  const dialogRef = useRef<HTMLDivElement>(null);
+  useOutsideClick(dialogRef, onClose);
+
   if (!isOpen) return null;
 
   return (
     <Portal portalId="dialog">
-      <div className="w-100 h-100 fixed flex items-center justify-center bg-overlay">
-        <div className="relative w-full rounded-lg bg-white px-6 py-8 shadow-normal">
+      <div className="fixed top-0 flex h-full w-full items-center justify-center bg-overlay">
+        <div
+          ref={dialogRef}
+          role="dialog"
+          aria-modal="true"
+          className={cn('relative rounded-lg bg-white px-6 py-8 shadow-normal', className)}
+        >
           {children}
         </div>
       </div>
