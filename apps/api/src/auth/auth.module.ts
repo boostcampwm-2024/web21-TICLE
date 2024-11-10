f1ec4e9f33b2da4daa16b17f079e52d0ab5ae41f
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-10 13:53:21 +0900

    feat: jwt type 파일 설치 및 전략 생성

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { UserModule } from '@/user/user.module';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './strategy/local.strategy';
+import { LocalStrategy } from './local/local.strategy';
 // import { GithubStrategy } from './strategy/github.strategy';
 // import { GoogleStrategy } from './strategy/google.strategy';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '999d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy], //, GithubStrategy, GoogleStrategy],
 })
