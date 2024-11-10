0faa5d5ad547d3a4d84ae45e4b391ce332641f7d
seongha <11pi885@gmail.com>
2024-11-10 20:24:50 +0900

    feat: ticle 생성 구현

--- apps/api/src/ticle/ticle.controller.ts
+++ apps/api/src/ticle/ticle.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+
+import { TicleService } from './ticle.service';
 
 @Controller('ticle')
 export class TicleController {
+  constructor(private readonly ticleService: TicleService) {}
+
   @Post()
-  createTicle() {}
+  async createTicle(@Body() createTicleDto: CreateTicleDto) {
+    const newTicle = await this.ticleService.createTicle(createTicleDto);
+    return newTicle.id;
+  }
 
   @Get(':ticleId')
   getTicle(@Param('ticleId') params: any) {}
@@ -17,3 +24,14 @@ export class TicleController {
   @Post(':ticleId/apply')
   applyToTicle(@Param('ticleId') params: any) {}
 }
+
+export type CreateTicleDto = {
+  speakerName: string;
+  speakerEmail: string;
+  speakerIntroduce: string;
+  title: string;
+  content: string;
+  startTime: Date;
+  endTime: Date;
+  tags?: string[];
+};
---
+++ apps/api/src/ticle/ticle.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Ticle, TicleStatus } from '@/entity/ticle.entity';
+
+import { CreateTicleDto } from './ticle.controller';
+
+@Injectable()
+export class TicleService {
+  constructor(
+    @InjectRepository(Ticle)
+    private ticleRepository: Repository<Ticle>
+  ) {}
+
+  async createTicle(createTicleDto: CreateTicleDto): Promise<Ticle> {
+    try {
+      const newTicle = this.ticleRepository.create({
+        ...createTicleDto,
+        ticleStatus: TicleStatus.OPEN,
+        applicants: [],
+        summary: null,
+        tags: [],
+      });
+
+      return await this.ticleRepository.save(newTicle);
+    } catch (error) {
+      throw new Error(`Failed to create ticle `);
+    }
+  }
+}
