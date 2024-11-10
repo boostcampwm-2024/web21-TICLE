75f93441bb2a23d8cdde91d0202202049e7ce420
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-10 16:46:39 +0900

    feat: svgr 세팅

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,10 @@
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), TanStackRouterVite({ routeToken: '_layout' })],
+  plugins: [react(), tsconfigPaths(), svgr(), TanStackRouterVite({ routeToken: '_layout' })],
 });
