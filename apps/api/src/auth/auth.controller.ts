f9e814955cfb152a32166a2039766a5bdac64a8e
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-10 13:30:27 +0900

    feat: controller, service의 로그인 로직 구현

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   @Post('signup')
   signup() {}
 
   @Post('login')
-  localLogin() {}
+  @UseGuards(LocalAuthGuard)
+  localLogin(@Request() req) {
+    return {
+      status: 'success',
+      data: this.authService.login(req.user),
+    };
+  }
 
   @Get('google/login')
   googleAuth() {}
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
+import { User } from '@/entity/user.entity';
 import { UserService } from '@/user/user.service';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
 
-  async validateUser(username: string, password: string): Promise<any> {
+  async validateUser(username: string, password: string): Promise<Omit<User, 'password'> | null> {
     const user = await this.userService.findUser(username); // user 태이블에 username이 있는지 확인, 있으면 user 객체 반환
     if (user) {
       const isPasswordValid = await bcrypt.compare(password, user.password);
@@ -17,5 +18,12 @@ export class AuthService {
       }
     }
     return null;
+  }
+
+  async login(user: Omit<User, 'password'>): Promise<{ access_token: string }> {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
