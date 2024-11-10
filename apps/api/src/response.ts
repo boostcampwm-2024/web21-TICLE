6e8198af5562c0809d3b0485b942a80b220c70c8
seongha <11pi885@gmail.com>
2024-11-10 23:46:21 +0900

    feat: api response template 생성

---
+++ apps/api/src/response.ts
@@ -0,0 +1,46 @@
+export interface SuccessResponse<T> {
+  status: 'success';
+  data: {
+    result: T;
+    message: string;
+  };
+}
+
+export interface ErrorResponse {
+  status: 'error';
+  error: {
+    code: number;
+    message: string;
+    details?: any;
+  };
+}
+
+export class Response {
+  static success<T>(
+    result: T,
+    message: string = '요청이 성공적으로 처리되었습니다.'
+  ): SuccessResponse<T> {
+    return {
+      status: 'success',
+      data: {
+        result,
+        message,
+      },
+    };
+  }
+
+  static error(
+    code: number = 400,
+    message: string = '유효하지 않은 요청입니다.',
+    details?: any
+  ): ErrorResponse {
+    return {
+      status: 'error',
+      error: {
+        code,
+        message,
+        details,
+      },
+    };
+  }
+}
