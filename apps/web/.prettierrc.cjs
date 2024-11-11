4eb80092b24d3e6973a7064cf647cad6670e8538
seoko97 <sukho1007@naver.com>
2024-11-11 14:43:29 +0900

    feat: prettier plugin cva 함수에 대한 lintting추가

--- apps/web/.prettierrc.cjs
+++ apps/web/.prettierrc.cjs
@@ -3,5 +3,5 @@ const rootConfig = require('../../.prettierrc.js');
 module.exports = {
   ...rootConfig,
   plugins: ['prettier-plugin-tailwindcss'],
-  tailwindFunctions: ['cn', 'clsx', 'twMerge'],
+  tailwindFunctions: ['cn', 'clsx', 'twMerge', 'cva'],
 };
