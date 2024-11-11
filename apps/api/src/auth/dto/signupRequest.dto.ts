b681520dca690da2b78dbb3e81264676faf8e24c
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-11 14:17:28 +0900

    feat: dto 필수정보 아닌 데이터 옵셔널 추가. swagger에도 반영

--- apps/api/src/auth/dto/signupRequest.dto.ts
+++ apps/api/src/auth/dto/signupRequest.dto.ts
@@ -13,15 +13,15 @@ export class SignupRequestDto {
   @ApiProperty({ example: 'user@example.com' })
   email: string;
 
-  @ApiProperty({ example: "Hello, I'm user" })
-  introduce: string;
+  @ApiProperty({ example: "Hello, I'm user", required: false })
+  introduce?: string;
 
-  @ApiProperty({ example: 'https://example.com/profile.jpg' })
-  profileImageUrl: string;
+  @ApiProperty({ example: 'https://example.com/profile.jpg', required: false })
+  profileImageUrl?: string;
 
-  @ApiProperty({ example: 'local' })
-  provider: string;
+  @ApiProperty({ example: 'local', required: false })
+  provider?: string;
 
-  @ApiProperty({ example: 'null' })
-  socialId: string;
+  @ApiProperty({ example: 'null', required: false })
+  socialId?: string;
 }
