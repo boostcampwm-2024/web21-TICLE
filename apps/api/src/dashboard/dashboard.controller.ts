5fbbc2e2e211abb1ca42848105d7e407ce89caa6
LEE JIEUN <jieun3595@soongsil.ac.kr>
2024-11-11 14:34:39 +0900

    feat: service에 getCreatedTicleList 구현

--- apps/api/src/dashboard/dashboard.controller.ts
+++ apps/api/src/dashboard/dashboard.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 import { DashboardService } from './dashboard.service';
 
@@ -7,7 +7,9 @@ export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
   @Get('created')
-  getCreatedTicleList() {}
+  getCreatedTicleList(@Body() body: { speakerId: number }) {
+    return this.dashboardService.getCreatedTicleList(body.speakerId);
+  }
 
   @Get('applied')
   getAppliedTicleList() {}
@@ -21,3 +23,18 @@ export class DashboardController {
   @Post('applied/:ticleId/join')
   joinTicle(@Param('ticleId') ticleId: number) {}
 }
+
+//select * from application where ticle_id = 1; // 참가자들 목록
+//select * from ticle where author_id = 1; // 내가 만든 티클 목록
+// ticleservice
+//dashboardservice -> (ticleRepository , ApplicantRepository)
+
+//dashboardController(/applied) -> dashboardService에 구현된 getAppliedTicles()  -> ApplicantRepository -> findAllByUserId()
+//dashboardController(/created)->  dashboardService에 구현된 getCreatedTicles()  -> TicleRepository -> findAllByAuthorId()
+
+// domain 이라는 패키지 안에  repository, entity
+// auth -> presentation , domain
+
+// 객체 객체에서 의존관게를 갖는 객체들 -> class 내에 있는field 변수들
+
+//controller -> service -> repository , service
--- apps/api/src/dashboard/dashboard.service.ts
+++ apps/api/src/dashboard/dashboard.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Applicant } from '@/entity/applicant.entity';
+import { Ticle } from '@/entity/ticle.entity';
 
 @Injectable()
 export class DashboardService {
-  getCreatedTicleList() {
-    return 'getCreatedTicleList';
+  @InjectRepository(Ticle)
+  private ticleRepository: Repository<Ticle>;
+  @InjectRepository(Applicant)
+  private applicantRepository: Repository<Applicant>;
+
+  async getCreatedTicleList(speakerId: number) {
+    try {
+      return await this.ticleRepository.find({
+        where: { speaker: { id: speakerId } },
+      });
+    } catch (error) {
+      throw new HttpException(`Failed to get created ticle list`, HttpStatus.BAD_REQUEST);
+    }
   }
 }
