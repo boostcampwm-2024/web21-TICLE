68af46c05a3af0511ea387e67f7b99823a2c9a7a
LEE JIEUN <jieun3595@soongsil.ac.kr>
2024-11-11 23:53:01 +0900

    feat: moduled에 Ticle과 Applicant import

--- apps/api/src/dashboard/dashboard.module.ts
+++ apps/api/src/dashboard/dashboard.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ticle } from '@/entity/ticle.entity';
+import { Applicant } from '@/entity/applicant.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Ticle, Applicant])],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
