f76839b62f0afd312d20625f4df758daa12c4143
seoko97 <sukho1007@naver.com>
2024-11-12 14:13:52 +0900

    feat: hover시 text color 색상 변경

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -26,9 +26,9 @@ const config: Config = {
       textColor: {
         main: 'var(--black)',
         alt: 'var(--grey-900)',
+        hover: 'var(--grey-800)',
         weak: 'var(--grey-700)',
         primary: 'var(--purple-500)',
-        hover: 'var(--purple-700)',
         white: 'var(--white)',
         error: 'var(--red)',
       },
