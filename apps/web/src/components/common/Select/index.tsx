8d01a14286f4a68ba85268af182a930dcb8dba1d
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-12 15:02:30 +0900

    feat: 선택된 option을 외부에서 관리하도록 구현

--- apps/web/src/components/common/Select/index.tsx
+++ apps/web/src/components/common/Select/index.tsx
@@ -8,23 +8,28 @@ import useOutsideClick from '@/hooks/useOutsideClick';
 interface Select {
   options: string[];
   placeholder: string;
+  selectedOption?: string;
+  onChange?: (value: string) => void;
 }
 
-function Select({ options, placeholder }: Select) {
+function Select({ options, placeholder, selectedOption, onChange }: Select) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [selectedOption, setSelectedOption] = useState<string>('');
 
-  const handleOptionClick = (option: string) => {
-    setSelectedOption(option);
+  const handleOptionChange = (option: string) => {
+    onChange?.(option);
     setIsOpen(false);
   };
 
-  const handleSelectOpen = () => {
+  const handleSelectClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleSelectToggle = () => {
     setIsOpen((prev) => !prev);
   };
 
   const selectRef = useRef<HTMLDivElement>(null);
-  useOutsideClick(selectRef, handleSelectOpen);
+  useOutsideClick(selectRef, handleSelectClose);
 
   const selectVariants = cva(
     'text-body1 cursor-pointer text-main px-3.5 py-2.5 flex gap-3.5 justify-between items-center rounded-base',
@@ -43,7 +48,7 @@ function Select({ options, placeholder }: Select) {
 
   return (
     <div ref={selectRef} className="relative max-w-32">
-      <label className={selectVariants({ isOpen: isOpen })} onClick={handleSelectOpen}>
+      <label className={selectVariants({ isOpen: isOpen })} onClick={handleSelectToggle}>
         {selectedOption || placeholder}
         {isOpen ? <ChevronUpIc /> : <ChevronDownIc />}
       </label>
@@ -53,7 +58,7 @@ function Select({ options, placeholder }: Select) {
             <li
               className="cursor-pointer rounded-base px-6 py-2.5 hover:bg-teritary"
               key={option}
-              onClick={() => handleOptionClick(option)}
+              onClick={() => handleOptionChange(option)}
             >
               {option}
             </li>
