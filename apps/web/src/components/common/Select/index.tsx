a98843580a1c8a9c53aeb35c9e77a5feb33a0e56
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-12 14:32:33 +0900

    feat: Select 스타일링

--- apps/web/src/components/common/Select/index.tsx
+++ apps/web/src/components/common/Select/index.tsx
@@ -1,4 +1,8 @@
+import { cva } from 'class-variance-authority';
 import { useState } from 'react';
+
+import ChevronDownIc from '@/assets/icons/chevron-down.svg?react';
+import ChevronUpIc from '@/assets/icons/chevron-up.svg?react';
 
 interface Select {
   options: string[];
@@ -18,13 +22,35 @@ function Select({ options, placeholder }: Select) {
     setIsOpen((prev) => !prev);
   };
 
+  const selectVariants = cva(
+    'text-body1 cursor-pointer text-main px-3.5 py-2.5 flex gap-3.5 justify-between items-center rounded-base',
+    {
+      variants: {
+        isOpen: {
+          true: 'border border-primary',
+          false: 'border border-main',
+        },
+      },
+      defaultVariants: {
+        isOpen: true,
+      },
+    }
+  );
+
   return (
-    <div>
-      <label onClick={handleSelectOpen}>{selectedOption || placeholder}</label>
+    <div className="min-w-28 max-w-fit">
+      <label className={selectVariants({ isOpen: isOpen })} onClick={handleSelectOpen}>
+        {selectedOption || placeholder}
+        {isOpen ? <ChevronUpIc /> : <ChevronDownIc />}
+      </label>
       {isOpen && (
-        <ul>
+        <ul className="flex flex-col items-center gap-1.5 rounded-base border border-main p-2">
           {options.map((option) => (
-            <li key={option} onClick={() => handleOptionClick(option)}>
+            <li
+              className="cursor-pointer rounded-base px-6 py-2.5 hover:bg-teritary"
+              key={option}
+              onClick={() => handleOptionClick(option)}
+            >
               {option}
             </li>
           ))}
