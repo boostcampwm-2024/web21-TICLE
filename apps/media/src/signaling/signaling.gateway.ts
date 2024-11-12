dc757b83ad8068ac08c9858ea3d06f1df24e3017
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-12 17:01:38 +0900

    fix: 불필요 코드 제거

--- apps/media/src/signaling/signaling.gateway.ts
+++ apps/media/src/signaling/signaling.gateway.ts
@@ -3,8 +3,5 @@ import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 @WebSocketGateway()
 export class SignalingGateway {
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    client.broadcast.emit('message', `server response : ${payload.message}`);
-    return `server response : ${payload.message}`;
-  }
+  handleMessage(client: any, payload: any) {}
 }
