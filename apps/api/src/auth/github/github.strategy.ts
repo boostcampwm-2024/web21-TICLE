729d301697e13b1169e421833abdf7a1cf72a1fb
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-13 23:05:30 +0900

    feat: github strategy 생성

---
+++ apps/api/src/auth/github/github.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-github2';
+
+@Injectable()
+export class GitHubStrategy extends PassportStrategy(Strategy, 'github') {
+  constructor(private configService: ConfigService) {
+    super({
+      clientID: configService.get<string>('GITHUB_CLIENT_ID'),
+      clientSecret: configService.get<string>('GITHUB_CLIENT_SECRET'),
+      callbackURL: configService.get<string>('GITHUB_CALLBACK_URL'),
+      scope: ['user:email'],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: Profile) {
+    const { id, username, emails, photos } = profile;
+
+    const user = {
+      provider: 'github',
+      socialId: id,
+      username: username,
+      email: emails[0].value,
+      profileImageUrl: photos?.[0]?.value,
+    };
+
+    return user;
+  }
+}
