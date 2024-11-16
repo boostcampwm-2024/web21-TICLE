f4265f2a166864c0d55f78890daab32167f1e593
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-16 16:30:11 +0900

    feat: 로컬로그인 id,pw null 검사 추가

--- apps/api/src/auth/local/local.strategy.ts
+++ apps/api/src/auth/local/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 
@@ -11,6 +11,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string) {
+    if (!username || !password) {
+      throw new UnauthorizedException('아이디와 비밀번호를 입력해주세요.');
+    }
     const user = await this.authService.validateLocalLogin(username, password);
     return user;
   }
