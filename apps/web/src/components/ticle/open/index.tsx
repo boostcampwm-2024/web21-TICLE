e7a169a36a3c661e918d37be969ce33fc2f20e74
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-17 03:37:23 +0900

    feat: error 여부에 따라 에러메시지 렌더링

--- apps/web/src/components/ticle/open/index.tsx
+++ apps/web/src/components/ticle/open/index.tsx
@@ -8,7 +8,13 @@ import TextInput from '@/components/common/TextInput';
 import FormBox from './FormBox';
 
 function Open() {
-  const { register, handleSubmit, errors } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
+  });
   const navigate = useNavigate();
 
   const onSubmit = () => {
@@ -18,33 +24,52 @@ function Open() {
   return (
     <div>
       <h1 className="mb-14 text-head1 text-main">티클 개설하기</h1>
-      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)} noValidate>
         <FormBox title="발표자 정보">
           <TextInput
             label="이름(닉네임)"
             required
             {...register('name', {
-              required: true,
-              maxLength: 10,
+              required: {
+                value: true,
+                message: '필수로 입력해 주세요.',
+              },
+              maxLength: {
+                value: 10,
+                message: '10자 이내로 입력해 주세요.',
+              },
             })}
+            errorMessage={errors.name?.message as string}
           />
           <TextInput
             label="이메일"
             type="email"
             required
             {...register('email', {
-              required: true,
+              required: {
+                value: true,
+                message: '필수로 입력해 주세요.',
+              },
             })}
+            errorMessage={errors.email?.message as string}
           />
           <TextArea
             label="자기소개"
             description="개설할 티클과 관련하여 자신에 대해 알리고 싶은 내용을 적어주세요."
-            size="sm"
+            size="md"
             required
-            {...register('self-introduction', {
-              required: true,
-              maxLength: 500,
+            maxLength={500}
+            {...register('selfIntroduction', {
+              required: {
+                value: true,
+                message: '필수로 입력해 주세요.',
+              },
+              maxLength: {
+                value: 500,
+                message: '500자 이내로 입력해 주세요.',
+              },
             })}
+            errorMessage={errors.selfIntroduction?.message as string}
           />
         </FormBox>
         <FormBox title="티클 정보" className="mt-14">
@@ -52,19 +77,34 @@ function Open() {
             label="제목"
             required
             {...register('title', {
-              required: true,
-              maxLength: 30,
+              required: {
+                value: true,
+                message: '필수로 입력해 주세요.',
+              },
+              maxLength: {
+                value: 30,
+                message: '30자 이내로 입력해 주세요.',
+              },
             })}
+            errorMessage={errors.title?.message as string}
           />
           <TextArea
             label="상세 설명"
             description="티클에 대한 자세한 정보를 적어주세요."
             size="sm"
             required
-            {...register('ticle-introduction', {
-              required: true,
-              maxLength: 1500,
+            maxLength={1500}
+            {...register('ticleIntroduction', {
+              required: {
+                value: true,
+                message: '필수로 입력해 주세요.',
+              },
+              maxLength: {
+                value: 1500,
+                message: '1500자 이내로 입력해 주세요.',
+              },
             })}
+            errorMessage={errors.ticleIntroduction?.message as string}
           />
           <TextInput label="해시태그" required />
         </FormBox>
