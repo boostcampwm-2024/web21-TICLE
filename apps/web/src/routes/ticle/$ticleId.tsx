c9e2f15f9cd4160b3a6316cb040bbff554c678d0
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-17 01:12:25 +0900

    feat: BasicLayout 컴포넌트 구현 및 필요한 페이지에 적용
    대시보드, 티클 개설, 티클 상세 페이지에 대해 공통된 레이아웃을 적용

--- apps/web/src/routes/ticle/$ticleId.tsx
+++ apps/web/src/routes/ticle/$ticleId.tsx
@@ -1,10 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router';
 
+import BasicLayout from '@/components/common/BasicLayout';
+
 export const Route = createFileRoute('/ticle/$ticleId')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { ticleId } = Route.useParams();
-  return `ticle ${ticleId}`;
+  return <BasicLayout></BasicLayout>;
 }
