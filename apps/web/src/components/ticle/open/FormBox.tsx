8de2ce7295e42f9bd570dc08a88ab8177159826c
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-17 03:13:46 +0900

    feat: 기본적인 react hook form validation 적용

--- apps/web/src/components/ticle/open/FormBox.tsx
+++ apps/web/src/components/ticle/open/FormBox.tsx
@@ -1,13 +1,21 @@
 import { ReactNode } from '@tanstack/react-router';
 
+import cn from '@/utils/cn';
+
 interface FormBoxProps {
   title?: string;
   children?: ReactNode;
+  className?: string;
 }
 
-function FormBox({ title, children }: FormBoxProps) {
+function FormBox({ title, children, className }: FormBoxProps) {
   return (
-    <section className="flex w-[49.5rem] flex-col gap-9 rounded-lg border border-main bg-white p-10 shadow-normal">
+    <section
+      className={cn(
+        'flex w-[49.5rem] flex-col gap-9 rounded-lg border border-main bg-white p-10 shadow-normal',
+        className
+      )}
+    >
       {title && <h3 className="text-head3 text-main">{title}</h3>}
       {children}
     </section>
--- apps/web/src/components/ticle/open/index.tsx
+++ apps/web/src/components/ticle/open/index.tsx
@@ -1,3 +1,6 @@
+import { useNavigate } from '@tanstack/react-router';
+import { useForm } from 'react-hook-form';
+
 import Button from '@/components/common/Button';
 import TextArea from '@/components/common/TextArea';
 import TextInput from '@/components/common/TextInput';
@@ -5,34 +8,73 @@ import TextInput from '@/components/common/TextInput';
 import FormBox from './FormBox';
 
 function Open() {
+  const { register, handleSubmit, errors } = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = () => {
+    navigate({ to: '/' });
+  };
+
   return (
     <div>
       <h1 className="mb-14 text-head1 text-main">티클 개설하기</h1>
-      <main className="flex flex-col gap-14">
+      <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
         <FormBox title="발표자 정보">
-          <TextInput label="이름(닉네임)" required />
-          <TextInput label="이메일" type="email" required />
+          <TextInput
+            label="이름(닉네임)"
+            required
+            {...register('name', {
+              required: true,
+              maxLength: 10,
+            })}
+          />
+          <TextInput
+            label="이메일"
+            type="email"
+            required
+            {...register('email', {
+              required: true,
+            })}
+          />
           <TextArea
             label="자기소개"
             description="개설할 티클과 관련하여 자신에 대해 알리고 싶은 내용을 적어주세요."
             size="sm"
             required
+            {...register('self-introduction', {
+              required: true,
+              maxLength: 500,
+            })}
           />
         </FormBox>
-        <FormBox title="티클 정보">
-          <TextInput label="제목" required />
+        <FormBox title="티클 정보" className="mt-14">
+          <TextInput
+            label="제목"
+            required
+            {...register('title', {
+              required: true,
+              maxLength: 30,
+            })}
+          />
           <TextArea
             label="상세 설명"
             description="티클에 대한 자세한 정보를 적어주세요."
             size="sm"
             required
+            {...register('ticle-introduction', {
+              required: true,
+              maxLength: 1500,
+            })}
           />
           <TextInput label="해시태그" required />
         </FormBox>
-      </main>
-      <div className="mt-9 flex w-full justify-end">
-        <Button size="lg">티클 개설하기</Button>
-      </div>
+
+        <div className="mt-9 flex w-full justify-end">
+          <Button size="lg" type="submit">
+            티클 개설하기
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
