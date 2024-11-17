a070edc2a0e384ba36095022b299a9a98161590d
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-18 00:15:57 +0900

    fix: GetUser 데코레이터가 무조건 id만 반환하게 변경

--- apps/api/src/common/decorator/get-user.decorator.ts
+++ apps/api/src/common/decorator/get-user.decorator.ts
@@ -2,19 +2,17 @@ import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@
 
 import { User } from '@/entity/user.entity';
 
-export const GetUser = createParamDecorator(
-  (data: keyof User | undefined, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    const user: User = request.user;
+export const GetUser = createParamDecorator((data: 'id' | undefined, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  const user: User = request.user;
 
-    if (!user) {
-      throw new UnauthorizedException('유저 정보가 존재하지 않습니다.');
-    }
-
-    if (data) {
-      return user[data];
-    }
-
-    return user;
+  if (!user) {
+    throw new UnauthorizedException('유저 정보가 존재하지 않습니다.');
   }
-);
+
+  if (data) {
+    return user[data];
+  }
+
+  return user;
+});
