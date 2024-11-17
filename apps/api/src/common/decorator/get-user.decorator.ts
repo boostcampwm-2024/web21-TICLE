fc6ff2a0ac81574c8a8dd69d19b9de39c1c32894
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-17 23:30:43 +0900

    feat: GetUser decorator 생성

---
+++ apps/api/src/common/decorator/get-user.decorator.ts
@@ -0,0 +1,20 @@
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+
+import { User } from '@/entity/user.entity';
+
+export const GetUser = createParamDecorator(
+  (data: keyof User | undefined, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user: User = request.user;
+
+    if (!user) {
+      throw new UnauthorizedException('유저 정보가 존재하지 않습니다.');
+    }
+
+    if (data) {
+      return user[data];
+    }
+
+    return user;
+  }
+);
