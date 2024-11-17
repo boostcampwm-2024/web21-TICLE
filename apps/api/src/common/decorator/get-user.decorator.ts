24e7f4c87119f1913c4d0bb2679297312588cb33
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-18 00:18:08 +0900

    fix: decorator 간소화 필요없는 기능은 아예 빼버림

--- apps/api/src/common/decorator/get-user.decorator.ts
+++ apps/api/src/common/decorator/get-user.decorator.ts
@@ -1,17 +1,11 @@
 import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 
-import { User } from '@/entity/user.entity';
-
-export const GetUser = createParamDecorator((data: 'id' | undefined, ctx: ExecutionContext) => {
+export const GetUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
-  const user: User = request.user;
+  const user = request.user.id;
 
   if (!user) {
     throw new UnauthorizedException('유저 정보가 존재하지 않습니다.');
-  }
-
-  if (data) {
-    return user[data];
   }
 
   return user;
