bdba0617434cb9b6ac19dbf10af309f07d68374e
seongha <11pi885@gmail.com>
2024-11-18 01:46:53 +0900

    fix: 에러 응답 인터페이스 제거

--- apps/api/src/response.interface.ts
+++ apps/api/src/response.interface.ts
@@ -2,12 +2,3 @@ export interface Response<T> {
   status: string;
   data: T;
 }
-
-export type ErrorResponse = {
-  status: 'error';
-  error: {
-    code: number;
-    message: string;
-    details?: any;
-  };
-};
