00e90289537d5f4d2f2735b34760f0fa9fdc1eb8
seoko97 <sukho1007@naver.com>
2024-11-18 13:32:30 +0900

    feat: dashboard mock handler 추가

--- apps/web/src/__mocks__/handlers/dashboard.ts
+++ apps/web/src/__mocks__/handlers/dashboard.ts
@@ -1,6 +1,16 @@
-export const getAppliedTicleList = async () => {};
+import { HttpResponse, HttpResponseResolver } from 'msw';
 
-export const getApplicants = async () => {};
+import applicantUserData from '@/__mocks__/data/dashboards/applicants-user.json';
+import ticleData from '@/__mocks__/data/dashboards/dashboard.json';
+
+export const getAppliedTicleList: HttpResponseResolver<{ ticleId: string }> = async () => {
+  return HttpResponse.json({ status: 'success', data: ticleData });
+};
+
+export const getApplicants = async () => {
+  // TODO: 맞는 유저 데이터를 가져오도록 수정
+  return HttpResponse.json({ status: 'success', data: applicantUserData });
+};
 
 export const startTicle = async () => {};
 
