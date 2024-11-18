5c4ab762c92e7e44cfb6f955096556efd1c2db30
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-18 23:22:04 +0900

    feat: useDevice type import 수정
    Co-authored-by: seoko97 <sukho1007@naver.com>

--- apps/web/src/hooks/mediasoup/useDevice.ts
+++ apps/web/src/hooks/mediasoup/useDevice.ts
@@ -1,12 +1,11 @@
 import * as mediasoup from 'mediasoup-client';
 import { useRef } from 'react';
-
-import { Device, RtpCapabilities } from '@/types/mediasoup';
+import type { client } from '@repo/mediasoup';
 
 const useDevice = () => {
-  const deviceRef = useRef<Device | null>(null);
+  const deviceRef = useRef<client.Device | null>(null);
 
-  const createDevice = async (rtpCapabilities: RtpCapabilities) => {
+  const createDevice = async (rtpCapabilities: client.RtpCapabilities) => {
     const device = new mediasoup.Device();
 
     await device.load({ routerRtpCapabilities: rtpCapabilities });
