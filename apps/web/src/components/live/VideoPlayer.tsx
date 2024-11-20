24887d0a9798d9070bfbfc159eccc8804c18608c
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-21 02:50:38 +0900

    feat: VideoPlayer에 마이크 on/off 표시와 이름 추가

--- apps/web/src/components/live/VideoPlayer.tsx
+++ apps/web/src/components/live/VideoPlayer.tsx
@@ -2,6 +2,10 @@
 import { cva } from 'class-variance-authority';
 import { useEffect, useRef, useState } from 'react';
 
+import MicOffIc from '@/assets/icons/mic-off.svg?react';
+import MicOnIc from '@/assets/icons/mic-on.svg?react';
+
+import Badge from '../common/Badge';
 import Loading from '../common/Loading/Loading';
 
 const videoVariants = cva('h-full w-full rounded-lg object-cover transition-opacity duration-300', {
@@ -16,12 +20,14 @@ const videoVariants = cva('h-full w-full rounded-lg object-cover transition-opac
   },
 });
 
-export interface MediaPlayerProps {
+export interface VideoPlayerProps {
   stream: MediaStream | null;
   muted?: boolean;
+  isMicOn?: boolean;
 }
 
-function VideoPlayer({ stream, muted = true }: MediaPlayerProps) {
+function VideoPlayer({ stream, muted = true, isMicOn = false }: VideoPlayerProps) {
+  const NAME = '김티클'; // TODO: 이름 받아오기
   const [isLoading, setIsLoading] = useState(true);
   const videoRef = useRef<HTMLVideoElement>(null);
 
@@ -38,11 +44,21 @@ function VideoPlayer({ stream, muted = true }: MediaPlayerProps) {
 
   return (
     <div className="relative h-full w-full overflow-hidden rounded-lg">
-      {isLoading && (
+      {isLoading ? (
         <div className="flex h-full items-center justify-center">
           <Loading />
         </div>
+      ) : (
+        <>
+          <div className="bg-altWeak absolute right-3 top-3 flex h-8 w-8 items-center justify-center rounded-full p-1">
+            {isMicOn ? <MicOnIc className="text-white" /> : <MicOffIc className="fill-white" />}
+          </div>
+          <div className="absolute bottom-3 left-3">
+            <Badge>{NAME}</Badge>
+          </div>
+        </>
       )}
+
       <video
         ref={videoRef}
         autoPlay
