6b284993310284c1941a44b932e39380905f17bb
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-20 23:07:55 +0900

    feat: 9명 이상 페이지네이션 구현

--- apps/web/src/components/live/index.tsx
+++ apps/web/src/components/live/index.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 
+import ChevronLeftIc from '@/assets/icons/chevron-left.svg?react';
+import ChevronRightIc from '@/assets/icons/chevron-right.svg?react';
 import useMediasoup from '@/hooks/mediasoup/useMediasoup';
 
 import AudioPlayer from './AudioPlayer';
 import VideoPlayer from './VideoPlayer';
+
+const ITEMS_PER_PAGE = 9;
+const VIDEO_GAP_SIZE = 20;
 
 const getColumnCount = (count: number) => {
   if (count <= 2) return count;
@@ -21,6 +26,7 @@ function MediaContainer() {
     closeStream,
   } = useMediasoup();
   const [isScreenSharing, setIsScreenSharing] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const toggleScreenShare = () => {
     if (isScreenSharing && screenStream) {
@@ -32,32 +38,63 @@ function MediaContainer() {
   };
 
   const videoStreams = remoteStreams.filter((stream) => stream.kind === 'video');
-  const columnCount = getColumnCount(videoStreams.length + 1);
-  const videoWidth = `calc((100% - ${(columnCount - 1) * 20}px) / ${columnCount})`;
+  const paginatedStreams = videoStreams.slice(
+    currentPage * (ITEMS_PER_PAGE - 1),
+    (currentPage + 1) * (ITEMS_PER_PAGE - 1)
+  );
+  const totalPages = Math.ceil((videoStreams.length + 1) / ITEMS_PER_PAGE);
+  const columnCount = getColumnCount(paginatedStreams.length + 1);
+  const videoWidth = `calc((100% - ${(columnCount - 1) * VIDEO_GAP_SIZE}px) / ${columnCount})`;
 
   return (
     <div className="fixed inset-0 flex flex-col justify-between bg-black px-32">
       <div className="flex h-full min-h-0 flex-1 items-center justify-center gap-5 rounded-lg">
         <div className="flex-1 overflow-hidden">
-          <div className="flex flex-wrap content-center justify-center gap-5">
-            <div className="aspect-video" style={{ width: videoWidth }}>
-              <VideoPlayer stream={videoStream} />
-            </div>
-            {remoteStreams.map((remoteStream) => (
+          <div
+            className="flex flex-wrap content-center justify-center"
+            style={{ gap: VIDEO_GAP_SIZE + 'px' }}
+          >
+            {currentPage === 0 && (
+              <div className="aspect-video" style={{ width: videoWidth }}>
+                <VideoPlayer stream={videoStream} />
+              </div>
+            )}
+
+            {paginatedStreams.map((stream) => (
               <div
-                key={remoteStream.socketId}
-                className={`aspect-video ${remoteStream.kind === 'audio' ? 'hidden' : ''}`}
+                key={stream.socketId}
+                className={`aspect-video ${stream.kind === 'audio' ? 'hidden' : ''}`}
                 style={{ width: videoWidth }}
               >
-                {remoteStream.kind === 'video' ? (
-                  <VideoPlayer stream={remoteStream.stream} />
+                {stream.kind === 'video' ? (
+                  <VideoPlayer stream={stream.stream} />
                 ) : (
-                  <AudioPlayer stream={remoteStream.stream} />
+                  <AudioPlayer stream={stream.stream} />
                 )}
               </div>
             ))}
           </div>
         </div>
+      </div>
+      <div className="fixed inset-0 flex w-full justify-between">
+        <button
+          type="button"
+          onClick={() => setCurrentPage((prev) => Math.max(0, prev - 1))}
+          style={{
+            opacity: currentPage === 0 ? 0 : '100%',
+          }}
+        >
+          <ChevronLeftIc />
+        </button>
+        <button
+          type="button"
+          onClick={() => setCurrentPage((prev) => Math.min(totalPages - 1, prev + 1))}
+          style={{
+            opacity: currentPage === totalPages - 1 && totalPages !== 0 ? 0 : '100%',
+          }}
+        >
+          <ChevronRightIc />
+        </button>
       </div>
 
       <footer className="flex h-[70px] justify-center gap-4 bg-primary pb-4 text-white">
