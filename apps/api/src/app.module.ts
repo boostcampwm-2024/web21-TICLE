21ba531c127fb34ba7fc4ccab2c0722cef138c09
seoko97 <sukho1007@naver.com>
2024-11-22 06:27:32 +0900

    feat: config 폴더가 src 내부에 존재하도록 변경

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { TypeOrmConfigService } from 'config/typeorm.config';
+import { TypeOrmConfigService } from '@/config/typeorm.config';
 
 import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
