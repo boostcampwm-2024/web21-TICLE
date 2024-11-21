d362353f87ac1a6ad59998c645b9e5415e1b9c12
seoko97 <sukho1007@naver.com>
2024-11-21 14:58:26 +0900

    feat: signOut API 추가

--- apps/web/src/api/auth.ts
+++ apps/web/src/api/auth.ts
@@ -1,26 +1,30 @@
 import axiosInstance from '@/api/axios';
 
-interface SignUpBody {
+type SignUpDto = {
   username: string;
   password: string;
   email: string;
   nickname?: string;
   introduce?: string;
   profileImageUrl?: string;
-}
+};
 
 const logIn = async () => {
   await axiosInstance.post('/auth/login');
 };
 
-const signUp = async (body: SignUpBody) => {
+const signUp = async (body: SignUpDto) => {
   const { data } = await axiosInstance.post('/auth/signup', body);
 
   return data;
+};
+
+const signOut = async () => {
+  await axiosInstance.post('/auth/logout');
 };
 
 const oauthLogin = async (provider: 'google' | 'github') => {
   await axiosInstance.get(`/auth/${provider}/login`);
 };
 
-export { logIn, signUp, oauthLogin };
+export { logIn, signUp, oauthLogin, signOut };
