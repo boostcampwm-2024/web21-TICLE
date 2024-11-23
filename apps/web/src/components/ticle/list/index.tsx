6927ff588213edff7edf9f84bd9a68c52877cbb2
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-24 02:54:51 +0900

    feat: Select를 통한 sort 구현

--- apps/web/src/components/ticle/list/index.tsx
+++ apps/web/src/components/ticle/list/index.tsx
@@ -1,6 +1,8 @@
 import { Link } from '@tanstack/react-router';
+import { useState } from 'react';
 
 import Loading from '@/components/common/Loading/Loading';
+import Select, { Option } from '@/components/common/Select';
 import { useTicleList } from '@/hooks/api/ticle';
 import { formatDateTimeRange } from '@/utils/date';
 
@@ -12,26 +14,52 @@ const getDateString = (startTime: string, endTime: string) => {
   return [dateStr, timeRangeStr].join(' ');
 };
 
+const SORT_OPTIONS: Option[] = [
+  {
+    label: '최신순',
+    value: 'newest',
+  },
+  {
+    label: '오래된순',
+    value: 'oldest',
+  },
+  {
+    label: '신청자순',
+    value: 'trending',
+  },
+];
+
 function TicleList() {
-  const { data: { ticles, meta } = { ticles: [], meta: {} }, isLoading } = useTicleList();
+  const [sortOption, setSortOption] = useState<Option>(SORT_OPTIONS[0] as Option);
+
+  const handleOptionChange = (option: Option) => {
+    setSortOption(option);
+  };
+
+  const { data: { ticles, meta } = { ticles: [], meta: {} }, isLoading } = useTicleList({
+    sort: sortOption.value as 'newest' | 'oldest' | 'trending',
+  });
 
   return (
-    <>
+    <div>
       <Banner />
-      <main className="my-12 flex flex-wrap justify-center gap-5">
-        {ticles.map((ticle) => (
-          <Link key={ticle.id} to="/ticle/$ticleId" params={{ ticleId: ticle.id.toString() }}>
-            <TicleCard
-              title={ticle.title}
-              tags={ticle.tags}
-              date={getDateString(ticle.startTime, ticle.endTime)}
-              speaker={ticle.speakerName}
-              applicantsCount={ticle.applicantsCount}
-            />
-          </Link>
-        ))}
-      </main>
-    </>
+      <div className="mt-14 flex w-[80rem] flex-col gap-12 justify-self-center">
+        <Select options={SORT_OPTIONS} selectedOption={sortOption} onChange={handleOptionChange} />
+        <main className="my-12 flex flex-wrap gap-5">
+          {ticles.map((ticle) => (
+            <Link key={ticle.id} to="/ticle/$ticleId" params={{ ticleId: ticle.id.toString() }}>
+              <TicleCard
+                title={ticle.title}
+                tags={ticle.tags}
+                date={getDateString(ticle.startTime, ticle.endTime)}
+                speaker={ticle.speakerName}
+                applicantsCount={ticle.applicantsCount}
+              />
+            </Link>
+          ))}
+        </main>
+      </div>
+    </div>
   );
 }
 
