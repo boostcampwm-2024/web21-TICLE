2f49e8193604e7667ecad29edc2b39c8bbf8e156
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-11-23 21:39:54 +0900

    feat: 로그인 정보 없을떄 에러추가, 토큰인증실패시 에러추가

--- apps/api/src/auth/jwt/jwt-auth.guard.ts
+++ apps/api/src/auth/jwt/jwt-auth.guard.ts
@@ -1,17 +1,25 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const req = context.switchToHttp().getRequest();
-
     const cookies = req.cookies;
-
     const token = cookies['accessToken'];
 
-    if (token) req.headers.authorization = `Bearer ${token}`;
+    if (!token) {
+      throw new UnauthorizedException('로그인이 필요합니다.');
+    }
+    req.headers.authorization = `Bearer ${token}`;
 
     return req;
+  }
+
+  handleRequest(err: any, user: any) {
+    if (err || !user) {
+      throw new UnauthorizedException('잘못된 인증 정보입니다.');
+    }
+    return user;
   }
 }
