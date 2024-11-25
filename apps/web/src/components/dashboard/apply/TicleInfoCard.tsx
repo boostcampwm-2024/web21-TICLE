aafaba9fcf10bf59ad91254abfae06421506ca92
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-26 00:44:23 +0900

    feat: 대시보드에서 티클 카드 클릭시 해당 티클 상세페이지로 이동

--- apps/web/src/components/dashboard/apply/TicleInfoCard.tsx
+++ apps/web/src/components/dashboard/apply/TicleInfoCard.tsx
@@ -23,25 +23,27 @@ function TicleInfoCard({
   const { dateStr, timeRangeStr } = formatDateTimeRange(startTime, endTime);
 
   return (
-    <div className="flex items-center justify-between rounded-lg border border-main bg-white p-6 shadow-normal">
-      <div className="flex gap-5">
-        <div className="flex items-center gap-3">
-          <h3 className="text-title2 text-main">개설자</h3>
-          <span className="w-36 text-body1 text-main">{speakerName}</span>
+    <Link to={`/ticle/${ticleId}`}>
+      <div className="flex items-center justify-between rounded-lg border border-main bg-white p-6 shadow-normal">
+        <div className="flex gap-5">
+          <div className="flex items-center gap-3">
+            <h3 className="text-title2 text-main">개설자</h3>
+            <span className="w-36 text-body1 text-main">{speakerName}</span>
+          </div>
+          <div className="flex items-center gap-3">
+            <h3 className="text-title2 text-main">티클명</h3>
+            <span className="w-80 text-body1 text-main">{ticleTitle}</span>
+          </div>
+          <div className="flex items-center gap-3">
+            <h3 className="text-title2 text-main">진행 일시</h3>
+            <span className="text-body1 text-main">{`${dateStr} ${timeRangeStr}`}</span>
+          </div>
         </div>
-        <div className="flex items-center gap-3">
-          <h3 className="text-title2 text-main">티클명</h3>
-          <span className="w-80 text-body1 text-main">{ticleTitle}</span>
-        </div>
-        <div className="flex items-center gap-3">
-          <h3 className="text-title2 text-main">진행 일시</h3>
-          <span className="text-body1 text-main">{`${dateStr} ${timeRangeStr}`}</span>
-        </div>
+        <Link to={`/live/${ticleId}`}>
+          <Button disabled={status === 'closed'}>티클 참여하기</Button>
+        </Link>
       </div>
-      <Link to={`/live/${ticleId}`}>
-        <Button disabled={status === 'closed'}>티클 참여하기</Button>
-      </Link>
-    </div>
+    </Link>
   );
 }
 
--- apps/web/src/components/dashboard/open/TicleInfoCard.tsx
+++ apps/web/src/components/dashboard/open/TicleInfoCard.tsx
@@ -19,40 +19,44 @@ interface TicleInfoCardProps {
 function TicleInfoCard({ ticleId, ticleTitle, startTime, endTime, status }: TicleInfoCardProps) {
   const { isOpen, onOpen, onClose } = useModal();
 
-  const { data: applicantsData, isLoading } = useApplicantsTicle(ticleId.toString());
+  const { data: applicantsData } = useApplicantsTicle(ticleId.toString());
   const { dateStr, timeRangeStr } = formatDateTimeRange(startTime, endTime);
 
   if (!applicantsData) return;
 
   return (
-    <div className="flex items-center justify-between rounded-lg border border-main bg-white p-6 shadow-normal">
-      <div className="flex gap-5">
-        <div className="flex items-center gap-3">
-          <h3 className="text-title2 text-main">티클명</h3>
-          <span className="w-80 text-body1 text-main">{ticleTitle}</span>
-        </div>
-        <div className="flex items-center gap-3">
-          <h3 className="text-title2 text-main">진행 일시</h3>
-          <span className="text-body1 text-main">{`${dateStr} ${timeRangeStr}`}</span>
-        </div>
-      </div>
-      <div className="flex gap-9">
-        <button
-          className="flex items-center gap-2 rounded-md p-2.5 hover:bg-teritary"
-          onClick={onOpen}
-        >
-          <span className="text-title2 text-primary">신청자 목록</span>
-
-          <div>
-            <PersonFilledIc className="fill-primary" />
+    <Link to={`/ticle/${ticleId}`}>
+      <div className="flex items-center justify-between rounded-lg border border-main bg-white p-6 shadow-normal">
+        <div className="flex gap-5">
+          <div className="flex items-center gap-3">
+            <h3 className="text-title2 text-main">티클명</h3>
+            <span className="w-80 text-body1 text-main">{ticleTitle}</span>
           </div>
-        </button>
-        <Link to={`/live/${ticleId}`}>
-          <Button disabled={status === 'closed'}>티클 시작하기</Button>
-        </Link>
+          <div className="flex items-center gap-3">
+            <h3 className="text-title2 text-main">진행 일시</h3>
+            <span className="text-body1 text-main">{`${dateStr} ${timeRangeStr}`}</span>
+          </div>
+        </div>
+        <div className="flex gap-9">
+          <button
+            className="flex items-center gap-2 rounded-md p-2.5 hover:bg-teritary"
+            onClick={onOpen}
+          >
+            <span className="text-title2 text-primary">신청자 목록</span>
+
+            <div>
+              <PersonFilledIc className="fill-primary" />
+            </div>
+          </button>
+          <Link to={`/live/${ticleId}`}>
+            <Button disabled={status === 'closed'}>티클 시작하기</Button>
+          </Link>
+        </div>
+        {isOpen && (
+          <ApplicantsDialog onClose={onClose} isOpen={isOpen} applicants={applicantsData} />
+        )}
       </div>
-      {isOpen && <ApplicantsDialog onClose={onClose} isOpen={isOpen} applicants={applicantsData} />}
-    </div>
+    </Link>
   );
 }
 
