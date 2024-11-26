7d490afbc35c1b7f8eeb445c47226bc019981994
LEE JIEUN <jieun3595@soongsil.ac.kr>
2024-11-27 02:31:45 +0900

    feat: 음성 내용 텍스트로 변환 기능 구현

--- apps/api/src/stream/stream.service.ts
+++ apps/api/src/stream/stream.service.ts
@@ -7,8 +7,6 @@ import { ErrorMessage } from '@repo/types';
 import { Summary } from '@/entity/summary.entity';
 import { Ticle } from '@/entity/ticle.entity';
 
-import { CreateSummaryDto } from './dto/createSummaryDto';
-
 @Injectable()
 export class StreamService {
   constructor(
@@ -19,8 +17,7 @@ export class StreamService {
     private configService: ConfigService
   ) {}
 
-  async createSummary(createSummaryDto: CreateSummaryDto) {
-    const { ticleId, audioUrl } = createSummaryDto;
+  async createSummary(ticleId: number, audioUrl: string, summaryText: string[]) {
     const ticle = await this.ticleRepository.findOne({
       where: { id: ticleId },
     });
@@ -30,10 +27,46 @@ export class StreamService {
     }
 
     const summary = this.summaryRepository.create({
-      name: ticle.title,
-      source: audioUrl,
+      audioUrl: audioUrl,
+      summaryText: summaryText,
       ticle: ticle,
     });
     return await this.summaryRepository.save(summary);
+  }
+
+  async transcribeAudio(remoteFileName: string) {
+    const speechEndpoint = this.configService.get<string>('CLOVASPEECH_ENDPOINT');
+
+    const body = {
+      dataKey: remoteFileName,
+      params: { enable: true },
+      language: 'ko-KR',
+      completion: 'sync',
+      fulltext: true,
+    };
+
+    try {
+      const response = await fetch(speechEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CLOVASPEECH-API-KEY': this.configService.get<string>('CLOVASPEECH_API_KEY'),
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        console.error('Error response:', errorText);
+        throw new Error(`Failed to transcribe audio: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      return data.text;
+    } catch (error) {
+      console.error('Failed to transcribe audio:', error);
+      throw new Error('Failed to transcribe audio with Clova Speech Recognition');
+    }
   }
 }
