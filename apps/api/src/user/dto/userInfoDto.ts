c6823904aa7fa3d543d4340b64e7d63a517c6757
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-26 15:33:49 +0900

    feat: userInfoDto 생성

---
+++ apps/api/src/user/dto/userInfoDto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserInfoDto {
+  @ApiProperty({
+    example: '1',
+    description: '유저 아이디',
+  })
+  id: number;
+
+  @ApiProperty({
+    example: 'simeunseo',
+    description: '유저 닉네임',
+  })
+  nickname: string;
+
+  @ApiProperty({
+    example: 'https://avatars.githubusercontent.com/u/55528304?v=4',
+    description: '유저 프로필 사진',
+  })
+  profileImageUrl: string;
+
+  @ApiProperty({
+    example: 'github',
+    description: '유저 소셜 로그인 출처',
+  })
+  provider: string;
+}
