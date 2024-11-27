fcb96ce078e7635bb9e41fbd5e0bb0cc8d4f5002
seoko97 <sukho1007@naver.com>
2024-11-28 00:38:11 +0900

    feat: producerId에 따라 consumer를 일시 정지, 재개 및 제거하는 메서드 추가

--- apps/media/src/room/peer.ts
+++ apps/media/src/room/peer.ts
@@ -53,6 +53,57 @@ export class Peer {
     return this.consumers.get(consumerId);
   }
 
+  deleteProducer(producerId: string) {
+    const producer = this.producers.get(producerId);
+
+    if (!producer) {
+      return;
+    }
+
+    producer.close();
+
+    this.producers.delete(producerId);
+  }
+
+  getConsumerByProducerId(producerId: string) {
+    const consumer = Array.from(this.consumers.values()).find(
+      (consumer) => consumer.producerId === producerId
+    );
+
+    return consumer;
+  }
+
+  pauseConsumerByProducerId(producerId: string) {
+    const consumer = this.getConsumerByProducerId(producerId);
+
+    if (!consumer) {
+      return;
+    }
+
+    consumer.pause();
+  }
+
+  resumeConsumerByProducerId(producerId: string) {
+    const consumer = this.getConsumerByProducerId(producerId);
+
+    if (!consumer) {
+      return;
+    }
+
+    consumer.resume();
+  }
+
+  removeConsumerByProducerId(producerId: string) {
+    const consumer = this.getConsumerByProducerId(producerId);
+
+    if (!consumer) {
+      return;
+    }
+
+    consumer.close();
+    this.consumers.delete(consumer.id);
+  }
+
   close() {
     this.consumers.forEach((consumer) => consumer.close());
     this.producers.forEach((producer) => producer.close());
