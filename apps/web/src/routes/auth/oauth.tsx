7e58b59ed55c7aecaebb316276fefb3d3f046697
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-11-28 14:07:50 +0900

    feat: /auth/oauth 에 redirect searchParam 포함

--- apps/web/src/routes/auth/oauth.tsx
+++ apps/web/src/routes/auth/oauth.tsx
@@ -2,7 +2,17 @@ import { createFileRoute } from '@tanstack/react-router';
 
 import Auth from '@/components/auth';
 
+interface SearchParams {
+  redirect: string | undefined;
+}
+
 export const Route = createFileRoute('/auth/oauth')({
+  validateSearch: (search: Record<string, unknown>): SearchParams => {
+    return {
+      redirect: typeof search.redirect === 'string' ? search.redirect : undefined,
+    };
+  },
+
   component: RouteComponent,
 });
 
