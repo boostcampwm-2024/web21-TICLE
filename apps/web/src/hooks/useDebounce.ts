57ca6bacb3eed53ef765d4a115b3db7d8bf8e7b9
seoko97 <sukho1007@naver.com>
2024-11-28 21:14:36 +0900

    feat: useDebouncedCallback 훅 추가

---
+++ apps/web/src/hooks/useDebounce.ts
@@ -0,0 +1,22 @@
+import { useRef, useCallback } from 'react';
+
+function useDebouncedCallback<T extends (...args: any[]) => void>(
+  callback: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  return useCallback(
+    (...args: Parameters<T>) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+      timeoutRef.current = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    },
+    [callback, delay]
+  );
+}
+
+export default useDebouncedCallback;
