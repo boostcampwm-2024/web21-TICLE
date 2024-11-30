3b22b0f093f983e56d1d29d1caaaaafc3d6e442e
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-12-01 01:16:10 +0900

    feat: getTicleList service에서 isOpen 필터링시 inprogress 상태인 것도 포함

--- apps/api/src/ticle/ticle.service.ts
+++ apps/api/src/ticle/ticle.service.ts
@@ -206,7 +206,7 @@ export class TicleService {
       .createQueryBuilder('ticle')
       .select('COUNT(*) as count')
       .where('ticle.ticleStatus = :status', {
-        status: isOpen ? TicleStatus.OPEN : TicleStatus.CLOSED,
+        status: isOpen ? TicleStatus.OPEN || TicleStatus.IN_PROGRESS : TicleStatus.CLOSED,
       });
     const totalTicleCount = await countQuery.getRawOne();
 
