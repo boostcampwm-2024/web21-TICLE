12af3aa7f916a3de6fa68f2208434b97521c64da
seoko97 <sukho1007@naver.com>
2024-12-02 03:32:30 +0900

    feat: 미디어 장치 관리 기능 추가 및 선택된 장치 ID 설정 로직 구현

---
+++ apps/web/src/hooks/useMediaTracks.ts
@@ -0,0 +1,151 @@
+import { useEffect, useState } from 'react';
+import { MediaTypes } from '@repo/mediasoup';
+
+import { LocalStream, MediaDevice } from '@/contexts/localStream/context';
+import { getCameraStream, getMicStream, getScreenStream } from '@/utils/stream';
+
+const DEFAULT_LOCAL_STREAM = {
+  stream: null,
+  paused: true,
+} as const;
+
+const getMediaDevices = (kind: MediaDeviceKind, devices: MediaDeviceInfo[]) => {
+  return devices
+    .filter((device) => device.kind === kind)
+    .map((device) => ({ label: device.label, deviceId: device.deviceId }));
+};
+
+const useMediaTracks = () => {
+  const [video, setVideo] = useState<LocalStream>({ ...DEFAULT_LOCAL_STREAM });
+  const [audio, setAudio] = useState<LocalStream>({ ...DEFAULT_LOCAL_STREAM });
+  const [screen, setScreen] = useState<LocalStream>({ ...DEFAULT_LOCAL_STREAM });
+
+  const [videoDevices, setVideoDevices] = useState<MediaDevice[]>([]);
+  const [audioDevices, setAudioDevices] = useState<MediaDevice[]>([]);
+  const [audioOutputDevices, setAudioOutputDevices] = useState<MediaDevice[]>([]);
+  const [selectedVideoDeviceId, setSelectedVideoDeviceId] = useState<string | null>(null);
+  const [selectedAudioDeviceId, setSelectedAudioDeviceId] = useState<string | null>(null);
+  const [selectedAudioOutputDeviceId, setSelectedAudioOutputDeviceId] = useState<string | null>(
+    null
+  );
+
+  const getCameraTrack = async () => {
+    if (video.stream || !selectedVideoDeviceId) {
+      return;
+    }
+
+    const stream = await getCameraStream({
+      video: { deviceId: selectedVideoDeviceId },
+    });
+
+    const track = stream.getVideoTracks()[0];
+
+    if (!track) {
+      return;
+    }
+
+    setVideo({ stream, paused: true });
+
+    return track;
+  };
+
+  const getAudioTrack = async () => {
+    if (audio.stream || !selectedAudioDeviceId) {
+      return;
+    }
+
+    const stream = await getMicStream({
+      audio: { deviceId: selectedAudioDeviceId },
+    });
+    const track = stream.getAudioTracks()[0];
+
+    if (!track) {
+      return;
+    }
+
+    setAudio({ stream, paused: true });
+
+    return track;
+  };
+
+  const getScreenTrack = async () => {
+    if (screen.stream) {
+      return;
+    }
+
+    const stream = await getScreenStream();
+
+    const track = stream.getVideoTracks()[0];
+
+    if (!track) {
+      return;
+    }
+
+    setScreen({ stream, paused: false });
+
+    return track;
+  };
+
+  const getMediaState = (type: MediaTypes) => {
+    if (type === 'video') {
+      return [video, setVideo] as const;
+    }
+
+    if (type === 'audio') {
+      return [audio, setAudio] as const;
+    }
+
+    if (type === 'screen') {
+      return [screen, setScreen] as const;
+    }
+
+    throw new Error('Invalid stream type');
+  };
+
+  useEffect(() => {
+    const fetchMediaDevices = async () => {
+      try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+
+        const videoInputs = getMediaDevices('videoinput', devices);
+        const audioInputs = getMediaDevices('audioinput', devices);
+        const audioOutputs = getMediaDevices('audiooutput', devices);
+
+        setVideoDevices(videoInputs);
+        setAudioDevices(audioInputs);
+        setAudioOutputDevices(audioOutputs);
+
+        if (videoInputs[0]) setSelectedVideoDeviceId(videoInputs[0].deviceId);
+        if (audioInputs[0]) setSelectedAudioDeviceId(audioInputs[0].deviceId);
+        if (audioOutputs[0]) setSelectedAudioOutputDeviceId(audioOutputs[0].deviceId);
+      } catch (error) {
+        console.error('Error fetching media devices:', error);
+      }
+    };
+
+    fetchMediaDevices();
+  }, []);
+
+  return {
+    video,
+    audio,
+    screen,
+    getCameraTrack,
+    getAudioTrack,
+    getScreenTrack,
+
+    videoDevices,
+    audioDevices,
+    audioOutputDevices,
+    selectedVideoDeviceId,
+    selectedAudioDeviceId,
+    selectedAudioOutputDeviceId,
+    setSelectedVideoDeviceId,
+    setSelectedAudioDeviceId,
+    setSelectedAudioOutputDeviceId,
+
+    getMediaState,
+  };
+};
+
+export default useMediaTracks;
