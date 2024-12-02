05c8c555f78fbfb095b617cce85cc96c2d46d9cb
seoko97 <sukho1007@naver.com>
2024-12-02 20:13:04 +0900

    feat: Toast 컴포넌트 추가
    - setTimeout을 통해 일정 시간 후에 closeToast 함수 호출

---
+++ apps/web/src/components/toast/Toast.tsx
@@ -0,0 +1,51 @@
+import { cva } from 'class-variance-authority';
+import { ReactNode, useEffect } from 'react';
+
+import ToastTransition from '@/components/toast/ToastTransition';
+import { ToastProps } from '@/core/toast/type';
+import useToast from '@/hooks/toast/useToast';
+
+const containerVariants = cva(
+  '[&.bounce-enter]:animate-bounce-in-bottom [&.bounce-exit]:animate-bounce-out-bottom z-0 mb-4 flex max-h-[800px] min-h-16 cursor-pointer justify-between overflow-hidden rounded-md bg-[#000000b3] text-white shadow-md',
+  {
+    variants: {
+      closeOnClick: {
+        true: 'pointer',
+        false: 'default',
+      },
+    },
+  }
+);
+const Toast = (props: ToastProps) => {
+  const { toastRef, eventHandlers } = useToast(props);
+  const { autoClose, children, closeToast, deleteToast, toastId, isIn, closeOnClick } = props;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (!isIn) return;
+
+      closeToast();
+    }, autoClose);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return (
+    <ToastTransition isIn={isIn} done={deleteToast} nodeRef={toastRef}>
+      <div
+        ref={toastRef}
+        id={String(toastId)}
+        className={containerVariants({ closeOnClick })}
+        {...eventHandlers}
+      >
+        <div className="mx-0 my-auto flex flex-1 items-center justify-center px-3 py-2 text-sm">
+          <div className="flex items-center justify-center gap-2">{children as ReactNode}</div>
+        </div>
+      </div>
+    </ToastTransition>
+  );
+};
+
+export default Toast;
