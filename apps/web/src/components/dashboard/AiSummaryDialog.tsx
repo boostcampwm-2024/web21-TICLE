5657c7677b11a9852a614b55be0f5e794b36a9e0
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-12-04 02:40:12 +0900

    feat: AI 요약이 완료되지 않았을 때 보여줄 로딩 뷰 구현

--- apps/web/src/components/dashboard/AiSummaryDialog.tsx
+++ apps/web/src/components/dashboard/AiSummaryDialog.tsx
@@ -1,6 +1,8 @@
 import { Dialog } from '@/components/common/Dialog';
 import { useAiSummary } from '@/hooks/api/dashboard';
 
+import Loading from '../common/Loading';
+
 interface AiSummaryDialogProps {
   isOpen: boolean;
   onClose: () => void;
@@ -15,7 +17,16 @@ function AiSummaryDialog({ isOpen, onClose, ticleId }: AiSummaryDialogProps) {
       <Dialog.Title align="center">AI 음성 요약</Dialog.Title>
       <Dialog.Close onClose={onClose} />
       <Dialog.Content className="custom-scrollbar overflow-y-scroll">
-        {data?.summary[0]}
+        {!data || data?.summary.length === 0 ? (
+          <div className="flex h-[20rem] w-full flex-col items-center justify-center gap-10">
+            <Loading color="primary" />
+            <span className="whitespace-pre text-center text-title1 text-primary">
+              AI 요약을 처리중이에요.
+            </span>
+          </div>
+        ) : (
+          <p className="whitespace-pre text-body1">{data?.summary[0]}</p>
+        )}
       </Dialog.Content>
     </Dialog.Root>
   );
