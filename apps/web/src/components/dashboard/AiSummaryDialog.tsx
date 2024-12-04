29a68efc6ddc9cdb9c9e1105d45f57b3723a868d
seoko97 <sukho1007@naver.com>
2024-12-04 19:18:59 +0900

    fix: AiSummaryDialog에서 데이터 유무에 따른 로딩 및 결과 메시지 처리 개선

--- apps/web/src/components/dashboard/AiSummaryDialog.tsx
+++ apps/web/src/components/dashboard/AiSummaryDialog.tsx
@@ -17,15 +17,23 @@ function AiSummaryDialog({ isOpen, onClose, ticleId }: AiSummaryDialogProps) {
       <Dialog.Title align="center">AI 음성 요약</Dialog.Title>
       <Dialog.Close onClose={onClose} />
       <Dialog.Content className="custom-scrollbar overflow-y-scroll">
-        {!data || data?.summary.length === 0 ? (
+        {!data && (
           <div className="flex h-[20rem] w-full flex-col items-center justify-center gap-10">
             <Loading color="primary" />
             <span className="whitespace-pre text-center text-title1 text-primary">
               AI 요약을 처리중이에요.
             </span>
           </div>
-        ) : (
-          <p className="whitespace-pre text-body1">{data?.summary[0]}</p>
+        )}
+        {data && !data.summaryText && (
+          <div className="flex h-[20rem] w-full flex-col items-center justify-center gap-10">
+            <span className="whitespace-pre text-center text-title1 text-primary">
+              AI 요약 결과가 없어요.
+            </span>
+          </div>
+        )}
+        {data && data.summaryText && (
+          <p className="whitespace-pre text-body1">{data.summaryText}</p>
         )}
       </Dialog.Content>
     </Dialog.Root>
