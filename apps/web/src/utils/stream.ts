950a94bbf97b1d7fe253cedb348d7e8e8f0047fe
seoko97 <sukho1007@naver.com>
2024-12-06 03:20:54 +0900

    refactor: simplify media stream constraints

--- apps/web/src/utils/stream.ts
+++ apps/web/src/utils/stream.ts
@@ -6,43 +6,21 @@ const DEFAULT_AUDIO_CONSTRAINTS: MediaTrackConstraints = {
   autoGainControl: true,
 };
 
-const DEFAULT_VIDEO_CONSTRAINTS: MediaTrackConstraints = {
-  width: { ideal: 1280, min: 640, max: 1920 },
-  height: { ideal: 720, min: 360, max: 1080 },
-  frameRate: { max: 30, ideal: 15, min: 15 },
-  aspectRatio: { ideal: 16 / 9 },
-};
-
-const DEFAULT_SCREEN_CONSTRAINTS: MediaTrackConstraints = {
-  width: { max: 1920, ideal: 1280 },
-  height: { max: 1080, ideal: 720 },
-  frameRate: { max: 30, ideal: 15 },
-};
-
 const getCameraStream = async (options: MediaTrackConstraints = {}) => {
   return navigator.mediaDevices.getUserMedia({
-    video: {
-      ...DEFAULT_VIDEO_CONSTRAINTS,
-      ...options,
-    },
+    video: { ...options },
   });
 };
 
 const getMicStream = async (options: MediaTrackConstraints = {}) => {
   return navigator.mediaDevices.getUserMedia({
-    audio: {
-      ...DEFAULT_AUDIO_CONSTRAINTS,
-      ...options,
-    },
+    audio: { ...DEFAULT_AUDIO_CONSTRAINTS, ...options },
   });
 };
 
 const getScreenStream = async (options: MediaTrackConstraints = {}) => {
   return navigator.mediaDevices.getDisplayMedia({
-    video: {
-      ...DEFAULT_SCREEN_CONSTRAINTS,
-      ...options,
-    },
+    video: { ...options },
   });
 };
 
