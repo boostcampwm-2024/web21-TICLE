9af890b751db7386cf205944c99ee807ddbf82ae
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-12-05 16:59:24 +0900

    feat: AI 요약 dialog UI 수정

--- apps/web/src/components/dashboard/AiSummaryDialog.tsx
+++ apps/web/src/components/dashboard/AiSummaryDialog.tsx
@@ -13,10 +13,10 @@ function AiSummaryDialog({ isOpen, onClose, ticleId }: AiSummaryDialogProps) {
   const { data } = useAiSummary(ticleId);
 
   return (
-    <Dialog.Root isOpen={isOpen} onClose={onClose} className="h-[30rem] w-[35rem]">
+    <Dialog.Root isOpen={isOpen} onClose={onClose} className="flex h-[30rem] w-[35rem] flex-col">
       <Dialog.Title align="center">AI 음성 요약</Dialog.Title>
       <Dialog.Close onClose={onClose} />
-      <Dialog.Content className="custom-scrollbar overflow-y-scroll">
+      <Dialog.Content className="custom-scrollbar h-full overflow-y-scroll">
         {!data && (
           <div className="flex h-[20rem] w-full flex-col items-center justify-center gap-10">
             <Loading color="primary" />
@@ -25,15 +25,8 @@ function AiSummaryDialog({ isOpen, onClose, ticleId }: AiSummaryDialogProps) {
             </span>
           </div>
         )}
-        {data && !data.summaryText && (
-          <div className="flex h-[20rem] w-full flex-col items-center justify-center gap-10">
-            <span className="whitespace-pre text-center text-title1 text-primary">
-              AI 요약 결과가 없어요.
-            </span>
-          </div>
-        )}
         {data && data.summaryText && (
-          <p className="whitespace-pre text-body1">{data.summaryText}</p>
+          <p className="flex-1 whitespace-pre-wrap text-body1">{data.summaryText}</p>
         )}
       </Dialog.Content>
     </Dialog.Root>
